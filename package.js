0c7ef0dcec68fb3a75fd0292b48b632a6472e545
Erickson Delgado <ericksond@gmail.com>
2015-02-16 21:28:40 -0500

    modified syntax in package.js

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ericksond:errors',
-  summary: 'A pattern to display application errors to the user'
+  summary: 'A pattern to display application errors to the user',
   version: '0.0.1',
   git: 'https://github.com/ericksond/meteor-errors.git',
   documentation: 'README.md'
