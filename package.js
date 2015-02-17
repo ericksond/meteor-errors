d36d306f09cf1b708b813d770de34b7285026d4f
Erickson Delgado <ericksond@gmail.com>
2015-02-16 22:03:14 -0500

    added errors_test.js

--- package.js
+++ package.js
@@ -15,10 +15,10 @@ Package.onUse(function(api, where) {
     api.export('Errors')
 });
 
-/*
+
 Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('ericksond:errors');
-  api.addFiles('ericksond:errors-tests.js');
+  api.use('ericksond:errors', 'client');
+  api.use(['tinytest', 'test-helpers'], 'client');
+
+  api.addFiles('errors_tests.js', 'client');
 });
-*/
