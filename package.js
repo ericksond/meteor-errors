625b115cc9ef63039990c286f554e2dca35368e0
Erickson Delgado <ericksond@gmail.com>
2015-04-08 14:15:47 -0400

    fixed package.js and published to atmosphere

--- package.js
+++ package.js
@@ -6,9 +6,8 @@ Package.describe({
   documentation: 'README.md'
 });
 
-Package.onUse(function(api, where) {
+Package.onUse(function(api) {
   api.versionsFrom('1.0.3.1');
-  api.use(['mongo', 'minimongo', 'templating'], 'client')
   api.addFiles(['errors.js', 'errors_list.jade', 'errors_list.html', 'errors_list.js'], 'client');
 
   if (api.export)
@@ -18,7 +17,5 @@ Package.onUse(function(api, where) {
 
 Package.onTest(function(api) {
   api.use('ericksond:errors', 'client');
-  api.use(['tinytest', 'test-helpers'], 'client');
-
   api.addFiles('errors_tests.js', 'client');
 });
